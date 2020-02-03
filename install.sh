f10295f6400de5a6e5d00bc87debe43686342964
lozovyy.a <aji136604@gmail.com>
2020-02-03 10:48:48 +0200

    Added jenkins install and basic conf

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 apt update
 
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 
-apt -y install php nginx mysql-server nodejs vsftpd
+apt -y install php nginx mysql-server nodejs vsftpd default-jre default-jdk jenkins
 # TODO: make silent install (no confirm request)
 npm install @angular/cli -g
+
+systemctl enable jenkins
+systemctl start jenkins

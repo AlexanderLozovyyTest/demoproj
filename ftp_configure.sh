4bad893cca885f94aef5734c94b085b59a2ee0ef
lozovyy.a <aji136604@gmail.com>
2020-02-12 20:28:34 +0200

    Added docs folder and changed fw conf

--- ftp_configure.sh
+++ ftp_configure.sh
@@ -21,17 +21,7 @@ sudo chown root:root /home/testuser
 # restart service in selected folder
 sudo service vsftpd restart
 
-# TODO: check if rules should be set before or after firewall restart
-# TODO: change ftp ports from 20,21 to allwed
-# TODO: move to separate file
-# add rules for used ports 
-sudo ufw allow 20/tcp, 21/tcp, 22, 80, 1024:1048, 8080, OpenSSH
-
-# restart firewall
-sudo ufw disable
+# firewall config
 sudo ufw enable
-
 sudo ufw allow 20/tcp, 21/tcp, 22, 80, 1024:1048, 8080, OpenSSH
-
-# check service status
-sudo ufw status
+sudo ufw reload
